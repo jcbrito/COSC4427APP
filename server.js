92fa19e2e4efea7b3839267d4fbaf545dc62f17c
jbrito <jcbritov2@gmail.com>
2020-08-05 13:30:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
     console.log('mongo db connection', err)
 })
 
-var server = http.listen(3000, () => {
+var server = http.listen(5000, () => {
     console.log('server is listening on port', server.address().port)
 })
