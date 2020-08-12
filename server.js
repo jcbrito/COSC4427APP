345f76e74bc8da1d6a6a2399b64583c754e72cb5
jbrito <jcbritov2@gmail.com>
2020-08-11 21:36:13 -0400

    CORS

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ io.on('connection', (socket) => {
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
