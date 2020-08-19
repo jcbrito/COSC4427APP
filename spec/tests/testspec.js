3a0c1e49823fcd0dc6db237f6c3189360f524b38
jbrito <jcbritov2@gmail.com>
2020-08-19 11:43:48 -0400

    updated reports

--- spec/tests/testspec.js
+++ spec/tests/testspec.js
@@ -1,7 +1,9 @@
 var reporters = require('jasmine-reporters');
 
 var junitReporter = new reporters.JUnitXmlReporter({
-  consolidateAll: true
+  savePath: 'reports',
+  filePrefix: 'my-jasmine-report.json',
+  consolidateAll: true  
 });
 
 jasmine.getEnv().addReporter(junitReporter);
@@ -33,4 +35,29 @@ describe('Tests', () =>{
 
     })
 
+    it('Get the sum of X and Y', () => {
+
+        // arrange
+        let num1 = 5;
+        let num2 = 3;
+
+        // act
+        const result = add(num1, num2);
+
+        // assert
+        expect(result).toBe(8);
+    })
+
+    it('get the difference of two numbers', () =>{
+
+        // arrange
+        let num1 = 5;
+        let num2 = 4;
+
+        // act
+        const result = sub(num1, num2);
+
+        // assert
+        expect(result).toBe(1);
+    })
 })
