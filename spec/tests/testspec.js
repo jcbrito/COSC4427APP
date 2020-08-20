167afcbcf1354553307ba784f08dc8b5bf64a02f
jbrito <jcbritov2@gmail.com>
2020-08-20 16:32:53 -0400

    added more tests

--- spec/tests/testspec.js
+++ spec/tests/testspec.js
@@ -1,6 +1,7 @@
 var reporters = require('jasmine-reporters');
-var mongoose = require('mongoose');
 require('dotenv').config();
+const {MongoClient} = require('mongodb');
+const client = new MongoClient(process.env.dbUrl);
 
 var junitReporter = new reporters.JUnitXmlReporter({
   savePath: 'reports',
@@ -10,23 +11,79 @@ var junitReporter = new reporters.JUnitXmlReporter({
 
 jasmine.getEnv().addReporter(junitReporter);
 
+async function listDatabases(client){
+
+  try{
+      await client.connect();
+      databasesList = await  client.db().admin().listDatabases();   
+  
+      return databasesList == null;
+  }
+  
+  catch(e){
+      return true;
+  }
+
+  finally{
+      client.close();
+  }
+};
+
+
+function testConnection(){
+  try{
+
+    client.connect();
+    return true;
+  }
+  catch(e){
+
+    return false;
+  }
+
+  finally{
+
+    client.close();
+  }
+}
+
+
 
 describe('Database tests', function (){
 
     it('Check if the connection is good', function(){
 
         // arrange
-        let tmp = null;
+       
+        let bool = false;
 
         // act
-        const conn = mongoose.createConnection(process.env.dbUrl);
 
+       bool = testConnection();
 
-        // assert
+       // assert
 
-        expect(conn).not.toBeNull;
+       expect(bool).toBeTrue();
+
     })
 
+    it('The connection is good, list databases', async function(){
+
+     // arrange
+
+     let bool = true;
+
+     // act
+      bool = await listDatabases(client);
+
+     // assert
+
+     expect(bool).toBeFalse();
+    
+
+    })
+
+  
 })
 
 
